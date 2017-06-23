1eac1767d21c4b4f230d1536a26975b8cee8b3c4
encadyma <encadyma@gmail.com>
2017-06-23 16:42:05 -0700

    Rebuild with Angular and node

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
 
+const HEROES: Hero[] = [  { id: 11, name: 'Mr. Nice' },
+                          { id: 12, name: 'Narco' },
+                          { id: 13, name: 'Bombasto' },
+                          { id: 14, name: 'Celeritas' },
+                          { id: 15, name: 'Magneta' },
+                          { id: 16, name: 'RubberMan' },
+                          { id: 17, name: 'Dynama' },
+                          { id: 18, name: 'Dr IQ' },
+                          { id: 19, name: 'Magma' },
+                          { id: 20, name: 'Tornado' }];
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  template: `
+    <h1>{{title}}</h1>
+    <h2>My Heroes</h2>
+    <md-list dense class="heroes">
+      <md-list-item *ngFor="let hero of heroes" (click)="onSelect(hero)">
+        <span class="badge">#{{hero.id}}</span> <button md-raised-button>{{hero.name}}</button>
+      </md-list-item>
+    </md-list>
+    <h2>{{hero.name}} details!</h2>
+    <div><label>id:</label>{{hero.id}}</div>
+    <div>
+      <md-input-container><input mdInput [(ngModel)]="hero.name" placeholder="Name"></md-input-container>
+    </div>
+  `
 })
 export class AppComponent {
-  title = 'app works!';
+  heroes=HEROES;
+
+  selectedHero: Hero;
+}
+
+export class Hero {
+  id:number;
+  name:string;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import 'hammerjs';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MdButtonModule, MdInputModule, MdListModule} from '@angular/material';
+
 
 import { AppComponent } from './app.component';
 
@@ -12,7 +17,11 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    BrowserAnimationsModule,
+    MdButtonModule,
+    MdInputModule,
+    MdListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
